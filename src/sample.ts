d5f99c371d6a7715e73ed90b26b76a1e1ddc5671
rapuhotelli <jukka@lahetkangas.net>
2021-12-22 22:20:42 +0200

    more or less done.

---
+++ src/sample.ts
@@ -0,0 +1,47 @@
+export const sample = "" +
+  "[17:19]You begin trial synthesis of a bottle of enchanted high durium ink.\n" +
+  "[17:19]You use Final Appraisal  Success!\n" +
+  "[17:19]Of the 30 parties currently recruiting, all match your search conditions.\n" +
+  "[17:19]You use Muscle Memory  Success!\n" +
+  "[17:19]   Progress increases by 651.\n" +
+  "[17:19]   Durability decreases by 10.\n" +
+  "[17:19]You use Manipulation  Success!\n" +
+  "[17:19]You use Waste Not II  Success!\n" +
+  "[17:19]You use Basic Synthesis  Success!\n" +
+  "[17:19]   Progress increases by 520.\n" +
+  "[17:19]   Durability decreases by 5.\n" +
+  "[17:19]You use Basic Touch  Success!\n" +
+  "[17:19]   Quality increases by 269.\n" +
+  "[17:19]   Durability decreases by 5.\n" +
+  "[17:19]You use Standard Touch  Success!\n" +
+  "[17:19]   Quality increases by 369.\n" +
+  "[17:19]   Durability decreases by 5.\n" +
+  "[17:19]You use Basic Touch  Success!\n" +
+  "[17:19]   Quality increases by 322.\n" +
+  "[17:19]   Durability decreases by 5.\n" +
+  "[17:19]You use Standard Touch  Success!\n" +
+  "[17:19]   Quality increases by 437.\n" +
+  "[17:19]   Durability decreases by 5.\n" +
+  "[17:19]You use Basic Touch  Success!\n" +
+  "[17:19]   Quality increases by 376.\n" +
+  "[17:19]   Durability decreases by 5.\n" +
+  "[17:19]You use Standard Touch  Success!\n" +
+  "[17:19]   Quality increases by 504.\n" +
+  "[17:19]   Durability decreases by 5.\n" +
+  "[17:19]You use Basic Touch  Success!\n" +
+  "[17:19]   Quality increases by 430.\n" +
+  "[17:19]   Durability decreases by 5.\n" +
+  "[17:19]You use Waste Not  Success!\n" +
+  "[17:19]You use Basic Touch  Success!\n" +
+  "[17:19]   Quality increases by 457.\n" +
+  "[17:19]   Durability decreases by 5.\n" +
+  "[17:19]You use Standard Touch  Success!\n" +
+  "[17:19]   Quality increases by 605.\n" +
+  "[17:19]   Durability decreases by 5.\n" +
+  "[17:20]You use Byregot's Blessing  Success!\n" +
+  "[17:20]   Quality increases by 1431.\n" +
+  "[17:20]   Durability decreases by 5.\n" +
+  "[17:20]You use Basic Synthesis  Success!\n" +
+  "[17:20]   Progress increases by 260.\n" +
+  "[17:20]   Durability decreases by 5.\n" +
+  "[17:20]Your trial synthesis of a bottle of enchanted high durium ink proved a success"
